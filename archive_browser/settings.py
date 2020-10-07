a22e40c097eb5425bd7e8f2dcd8266d8f9ea44e0
Richard Smith <richard.d.smith@stfc.ac.uk>
2020-10-07 15:15:47 +0100

    Update settings.py
    Removed all root directory filters

--- archive_browser/settings.py
+++ archive_browser/settings.py
@@ -86,7 +86,7 @@ USE_FTP = False
 DIRECTORY_INDEX = 'ceda-dirs'
 FILE_INDEX = 'ceda-fbi'
 MAX_FILES_PER_PAGE = 2000
-ROOT_DIRECTORY_FILTER = ['/sparc','/edc']
+ROOT_DIRECTORY_FILTER = []
 
 from archive_browser.settings_local import *
 
