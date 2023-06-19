13ea90c47e062bcb0622d2a7967b27dede77060f
dwest77a <daniel.westwood@stfc.ac.uk>
2023-06-19 11:48:39 +0100

    Updated data centre references

--- archive_browser/settings.py
+++ archive_browser/settings.py
@@ -53,7 +53,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'browser.context_processor.data_centre',
+                'fwtheme_django_ceda_serv.context_processors.data_centre',
                 'fwtheme_django_ceda_serv.context_processors.beacon',
             ],
         },
@@ -81,6 +81,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR + "/static"]
+STATIC_ROOT = BASE_DIR + "/staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 THREDDS_SERVICE = 'https://dap.ceda.ac.uk'
 FTP_SERVICE = 'ftp://ftp.ceda.ac.uk'
 USE_FTP = False
@@ -96,15 +100,3 @@ ACCESSCTL_URL = "https://accessctl.ceda.ac.uk/api/v1/rules/bypath/?format=json&p
 from archive_browser.settings_local import *
 
 CONTAINER_FLUID = False
-
-#NERC Datacentres - non CEDA Data Centres
-DATACENTRES = {
-    'pdc': 'pdc.html',
-    'bodc': 'bodc.html',
-    'eich': 'eich.html',
-    'ngdc': 'ngdc.html',
-    'ssdc': 'ssdc.html',
-    'sparc': 'sparc.html',
-    'edc': 'edc.html',
-    'eds': 'eds.html'
-}
