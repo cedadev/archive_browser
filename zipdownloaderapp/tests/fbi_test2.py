2845f0fc911936eb81e4bb26c30a692e2fac6c1d
Andrew Harwood <andrew.harwood@stfc.ac.uk>
2026-08-28 11:28:23 +0100

    update

--- zipdownloaderapp/tests/fbi_test2.py
+++ zipdownloaderapp/tests/fbi_test2.py
@@ -8,6 +8,10 @@ from datetime import datetime
 start = datetime.now()
 
 TOP_DIR = "/badc/acsoe"
+
+MAX_FILES = 1000000000
+MAX_SIZE = 100000000000000
+MAX_DEPTH = 10
 GREP = ""
 regex = f'.*{GREP}.*'
 
@@ -17,17 +21,70 @@ total_size = 0
 nfiles = 0
 
 
-#for rec in fbi_core.fbi_listdir(TOP_DIR, fetch_size=10000, dirs_only=False, removed=False, hidden=False):
+def get_fbi_records (top_dir, query_string, depth):
 
-for rec in fbi_core.fbi_records_under(path=TOP_DIR, include_removed=False, name_regex='acsoe', item_type='dir', fetch_size=1000, exclude_phenomena=False):
-    print (rec['path'], rec['type'])
+    top_dir.rstrip('/')
+
+    file_recs = []
+    total_size = 0
+    status = 'OK'
+
+    top_dir_path_depth = top_dir.count('/')
+
+    for rec in fbi_core.fbi_records_under(path=top_dir, include_removed=False, item_type='file', exclude_phenomena=True):
+
+        file_depth = rec['directory'].count('/') - top_dir_path_depth
+        rec['depth'] = file_depth
+
+        if query_string:
+            print ('checking')
+            if not query_string in rec['name']:
+                continue
+
+        if file_depth > depth:
+            continue
+
+        if len(file_recs) >= MAX_FILES:
+            status = 'max_files_exceeded'
+            break
+        if total_size >= MAX_SIZE:
+            status = 'max_size_exceeded'
+            break
+
+        file_recs.append(rec)
+        total_size = total_size + rec['size']
+
+    return (status, total_size, file_recs)
+
+
+(status, total_size, file_recs) = get_fbi_records (TOP_DIR, '', 10)
+
+n = 1
+for rec in file_recs:
+    print (n, rec['path'], rec['name'], rec['size'], rec['depth'])
+    n = n + 1
+
+print ('Status: ', status)
+print ('Size: ', total_size)
+print ('Length: ', len(file_recs))
+
+
+#
+# for rec in fbi_core.fbi_listdir(TOP_DIR, fetch_size=10000, dirs_only=False, removed=False, hidden=False):
+#     if rec['type'] != 'file':
+#         continue
+
+#for rec in fbi_core.ls_query(TOP_DIR, item_type='file', include_removed=False, exclude_phenomena=False):
+
+#for rec in fbi_core.fbi_records_under(path=TOP_DIR, include_removed=False, item_type='file', exclude_phenomena=False):
+#    print (rec['path'], rec['name'], rec['type'], rec['name'], rec['size'])
 #    total_size = total_size + rec['size']
-    nfiles = nfiles + 1
+#    nfiles = nfiles + 1
 
-natural_size = humanize.naturalsize(total_size)
-binary_size = humanize.naturalsize(total_size, binary=True)
+# natural_size = humanize.naturalsize(total_size)
+# binary_size = humanize.naturalsize(total_size, binary=True)
 
-print ('Files: ', nfiles)
-print ('Total size: ', natural_size, binary_size, total_size)
+# print ('Files: ', nfiles)
+# print ('Total size: ', natural_size, binary_size, total_size)
 
 print('Time: ', (datetime.now() - start).total_seconds())
